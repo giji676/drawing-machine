a7937ed505cd37fd471fb357c693bf39588c73dc
giji676 <tvtengoxachidze@gmail.com>
2023-05-16 21:33:47 +0100

    Optimisations

--- mainDriverV2.py
+++ mainDriverV2.py
@@ -3,69 +3,42 @@ import threading
 import math
 import time
 
-GPIO.cleanup()
-motorPins2 = (31, 33, 35, 37)
-motorPins1 = (12, 16, 18, 22)    # define pins connected to four phase ABCD of stepper motor
-CCWStep = (0x01,0x02,0x04,0x08) # define power supply order for rotating anticlockwise
-CWStep = (0x08,0x04,0x02,0x01)  # define power supply order for rotating clockwise
+motor_pins2 = (31, 33, 35, 37)
+motor_pins1 = (12, 16, 18, 22)
+CCW_step = (0x01, 0x02, 0x04, 0x08)
+CW_step = (0x08, 0x04, 0x02, 0x01)
+btns = [7, 11, 13, 15]
+leds = [36, 38, 40]
 
-btns = [7,11,13,15]
-leds = [36,38,40]
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(motor_pins1 + motor_pins2, GPIO.OUT)
+GPIO.setup(btns, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(leds, GPIO.OUT, initial=GPIO.LOW)
 
-def setup():
-    GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
-    for pin in motorPins1:
-        GPIO.setup(pin,GPIO.OUT)
-    for pin in motorPins2:
-        GPIO.setup(pin, GPIO.OUT)
-    for btn in btns:
-        GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    for led in leds:
-        GPIO.setup(led, GPIO.OUT)
-        GPIO.output(led, GPIO.LOW)
 
-# as for four phase stepping motor, four steps is a cycle. the function is used to drive the stepping motor clockwise or anticlockwise to take four steps
-def moveOnePeriod1(direction,ms):
-    for j in range(0,4,1):      # cycle for power supply order
-        for i in range(0,4,1):  # assign to each pin
-            if (direction == 1):# power supply order clockwise
-                GPIO.output(motorPins1[i],((CCWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
-            else :              # power supply order anticlockwise
-                GPIO.output(motorPins1[i],((CWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
-        if(ms<3):       # the delay can not be less than 3ms, otherwise it will exceed speed limit of the motor
-            ms = 3
-        time.sleep(ms*0.001)
+def moveOnePeriod(pins, direction, ms):
+    for j in range(0, 4):
+        for i, pin in enumerate(pins):
+            if direction == 1:
+                GPIO.output(pin, CCW_step[j] == 1 << i)
+            else:
+                GPIO.output(pin, CW_step[j] == 1 << i)
+        time.sleep(ms / 1000)
 
-def moveOnePeriod2(direction,ms):
-    for j in range(0,4,1):      # cycle for power supply order
-        for i in range(0,4,1):  # assign to each pin
-            if (direction == 1):# power supply order clockwise
-                GPIO.output(motorPins2[i],((CCWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
-            else :              # power supply order anticlockwise
-                GPIO.output(motorPins2[i],((CWStep[j] == 1<<i) and GPIO.HIGH or GPIO.LOW))
-        if(ms<3):       # the delay can not be less than 3ms, otherwise it will exceed speed limit of the motor
-            ms = 3
-        time.sleep(ms*0.001)
 
-# continuous rotation function, the parameter steps specifies the rotation cycles, every four steps is a cycle
-def moveSteps1(direction, ms, steps):
-    for i in range(steps):
-        moveOnePeriod1(direction, ms)
+def moveSteps(pins, direction, ms, steps):
+    for _ in range(steps):
+        moveOnePeriod(pins, direction, ms)
 
-def moveSteps2(direction, ms, steps):
-    for i in range(steps):
-        moveOnePeriod2(direction, ms)
-# function used to stop motor
-def motorStop1():
-    for i in range(0,4,1):
-        GPIO.output(motorPins1[i],GPIO.LOW)
 
-def motorStop2():
-    for i in range(0,4,1):
-        GPIO.output(motorPins2[i],GPIO.LOW)
+def motorStop(pins):
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
+
 
 def remap(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
 
 ###################################################################################
 
@@ -183,7 +156,6 @@ def destroy():
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
     start_time = time.time()
-    setup()
     max_x = max(row[0] for row in imgs)
     min_y = max(row[1] for row in imgs)
     
@@ -195,26 +167,26 @@ if __name__ == '__main__':     # Program entrance
     while pen_setup:
         if not(GPIO.input(btns[1])) and not(GPIO.input(btns[2])):
             m1 = [1, 3, 5]
-            th1 = threading.Thread(target=moveSteps1, args=(m1[0],m1[1],m1[2],))
+            th1 = threading.Thread(target=moveSteps, args=(motor_pins1,m1[0],m1[1],m1[2],))
             th1.start()
             th1.join()
             GPIO.output(leds[0], GPIO.HIGH)
         elif not(GPIO.input(btns[1])) and not(GPIO.input(btns[3])):
             m2 = [0, 3, 5]
-            th2 = threading.Thread(target=moveSteps2, args=(m2[0],m2[1],m2[2],))
+            th2 = threading.Thread(target=moveSteps, args=(motor_pins2,m2[0],m2[1],m2[2],))
             th2.start()
             th2.join()
             GPIO.output(leds[0], GPIO.HIGH)
         else:
             if not(GPIO.input(btns[2])):
                 m1 = [0, 3, 5]
-                th1 = threading.Thread(target=moveSteps1, args=(m1[0],m1[1],m1[2],))
+                th1 = threading.Thread(target=moveSteps, args=(motor_pins1,m1[0],m1[1],m1[2],))
                 th1.start()
                 th1.join()
                 GPIO.output(leds[0], GPIO.HIGH)
             if not(GPIO.input(btns[3])):
                 m2 = [1, 3, 5]
-                th2 = threading.Thread(target=moveSteps2, args=(m2[0],m2[1],m2[2],))
+                th2 = threading.Thread(target=moveSteps, args=(motor_pins2,m2[0],m2[1],m2[2],))
                 th2.start()
                 th2.join()
                 GPIO.output(leds[0], GPIO.HIGH)
@@ -242,20 +214,20 @@ if __name__ == '__main__':     # Program entrance
                 m2 = [values[0][1], values[1][1], values[2][1]]
 
                 if (m1[2] == 0):
-                    th2 = threading.Thread(target=moveSteps2, args=(m2[0],m2[1],m2[2],))
+                    th2 = threading.Thread(target=moveSteps, args=(motor_pins2,m2[0],m2[1],m2[2],))
                     th2.start()
                     th2.join()
                     time.sleep(m2[1] * m2[2])
                     
                 elif (m2[2] == 0):
-                    th1 = threading.Thread(target=moveSteps1, args=(m1[0],m1[1],m1[2],))
+                    th1 = threading.Thread(target=moveSteps, args=(motor_pins1,m2[0],m2[1],m2[2],))
                     th1.start()
                     th1.join()
                     time.sleep(m1[1] * m1[2])
                 
                 else:
-                    th1 = threading.Thread(target=moveSteps1, args=(m1[0],m1[1],m1[2],))
-                    th2 = threading.Thread(target=moveSteps2, args=(m2[0],m2[1],m2[2],))
+                    th1 = threading.Thread(target=moveSteps, args=(motor_pins1,m2[0],m2[1],m2[2],))
+                    th2 = threading.Thread(target=moveSteps, args=(motor_pins2,m2[0],m2[1],m2[2],))
                     th1.start()
                     th2.start()
                     th1.join()
